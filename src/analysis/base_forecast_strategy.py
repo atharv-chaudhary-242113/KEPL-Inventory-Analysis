047fcbcb0e258b12e2de6b4ece0db25a1e2fba30
Atharv Chaudhary <chaudharyatharv79@gmail.com>
2026-05-13 12:17:53 +0530

    security: remove vulnerable files

--- src/analysis/base_forecast_strategy.py
+++
@@ -1,8 +0,0 @@
-# src/analysis/base_forecast_strategy.py
-from abc import ABC, abstractmethod
-import pandas as pd
-
-class BaseForecastStrategy(ABC):
-    @abstractmethod
-    def compute(self, aggregated_df: pd.DataFrame, closing_stock_df: pd.DataFrame = None) -> pd.DataFrame:
-        pass
--- src/loaders/base_loader.py
+++
@@ -1,8 +0,0 @@
-# src/loaders/base_loader.py
-from abc import ABC, abstractmethod
-import pandas as pd
-
-class BaseLoader(ABC):
-    @abstractmethod
-    def load(self, filepath: str) -> pd.DataFrame:
-        pass
